1142c6ffa9ffbd7eceea7813c2e1be8ce7f1b485
willardshikami <willardshikami@gmail.com>
2018-12-06 10:50:22 +0300

    form data & class bindings, input validation et al

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tdf';
+  topics = [ 'Angular', 'React', 'Vue' ];
+
+  userModel = new User('Tom', 'tom@tom.com', 5554, '', 'Morning', true);
 }
